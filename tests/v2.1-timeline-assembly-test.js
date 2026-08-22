08776192beef2d533af264271f2cb10c9426c7ba
Pryshvitsyn <pryshvitsyna@gmail.com>
2026-08-22 10:20:37 +0200

    test(v2.1): assert clip boundary by id

--- tests/v2.1-timeline-assembly-test.js
+++ tests/v2.1-timeline-assembly-test.js
@@ -31,7 +31,7 @@ function main() {
 
   assert.equal(timeline.durationMs, 5000);
   assert.deepEqual(timeline.tracks, ['video-main', 'voice-main', 'music-main']);
-  assert.equal(timeline.clips[0].endMs, 3000);
+  assert.equal(timeline.clips.find((clip) => clip.id === 'shot-1-video').endMs, 3000);
 
   const assembled = assembleMedia({
     timeline,
