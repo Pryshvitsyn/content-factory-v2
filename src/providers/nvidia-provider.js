6a2d514eea510c08e9334e4f1d51841685190915
Pryshvitsyn <pryshvitsyna@gmail.com>
2026-08-22 14:14:44 +0200

    feat(v2.1): compose NVIDIA capabilities behind one provider

---
+++ src/providers/nvidia-provider.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { createNvidiaAdapter } = require('./nvidia-adapter');
+const { createNvidiaVideoAdapter } = require('./nvidia-video-adapter');
+
+function createNvidiaProvider(options = {}) {
+  const textAdapter = options.textAdapter || createNvidiaAdapter(options);
+  const videoAdapter = options.videoAdapter || createNvidiaVideoAdapter(options);
+
+  return Object.freeze({
+    provider: 'nvidia',
+    model: textAdapter.model,
+
+    supports({ capability, model } = {}) {
+      if (capability === 'text-generation') {
+        return !model || model === textAdapter.model;
+      }
+      if (capability === 'video-generation') {
+        return videoAdapter.supports({ capability, model });
+      }
+      return false;
+    },
+
+    async generate({ capability = 'text-generation', ...request } = {}) {
+      if (capability === 'text-generation') return textAdapter.generate(request);
+      if (capability === 'video-generation') return videoAdapter.generate(request);
+      throw new Error(`NVIDIA provider does not support capability '${capability}'`);
+    },
+  });
+}
+
+module.exports = { createNvidiaProvider };
