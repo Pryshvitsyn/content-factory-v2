b75ada2d1e597ae0130f8fa2d4bbcf29c379569b
Pryshvitsyn <pryshvitsyna@gmail.com>
2026-08-30 00:35:27 +0200

    V2.10.1: mount safe quality recovery console

--- apps/dashboard/client/src/main.jsx
+++ apps/dashboard/client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { QualityRecoveryConsole } from './QualityRecoveryConsole';
 import './styles.css';
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
+createRoot(document.getElementById('root')).render(<React.StrictMode><App /><QualityRecoveryConsole /></React.StrictMode>);
