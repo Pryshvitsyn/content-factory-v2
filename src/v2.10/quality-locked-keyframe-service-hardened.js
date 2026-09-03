e83fe3e9faa230341247cd938c2350d21e13fa1f
Pryshvitsyn <pryshvitsyna@gmail.com>
2026-09-03 10:07:44 +0200

    Return exact workflow identity for human pilot review

---
+++ src/v2.10/quality-locked-keyframe-service-hardened.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { QualityLockedKeyframeService } = require('./quality-locked-keyframe-service');
+
+class HardenedQualityLockedKeyframeService extends QualityLockedKeyframeService {
+  async startFirstVideo(args) {
+    const result = await super.startFirstVideo(args);
+    const scope = await this.scope(args.brandId);
+    const workflow = await this.repository.getLockedWorkflow({ draftId: args.id, ...scope });
+    return Object.freeze({
+      ...result,
+      workflowId: workflow?.id || null,
+      workflowState: workflow?.state || null,
+      readyForContinuationPreflight: false,
+      humanPilotApprovalRequired: result.accepted === true,
+      nextRequiredAction: result.accepted ? 'APPROVE_LOOK_AND_MOTION' : 'STOP_AND_REVIEW_FAILURE',
+      remainingProductionScheduled: false,
+      humanApprovalRequired: true,
+      autoPublish: false,
+    });
+  }
+}
+
+module.exports = { HardenedQualityLockedKeyframeService };
