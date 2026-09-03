ffffca81c083f79c8b3eb79c8f5b0af2e4fd3def
Pryshvitsyn <pryshvitsyna@gmail.com>
2026-09-03 21:19:38 +0200

    Render persisted keyframe validation console

--- apps/dashboard/client/src/main.jsx
+++ apps/dashboard/client/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { QualityRecoveryConsole } from './QualityRecoveryConsole';
+import { KeyframeValidationConsole } from './KeyframeValidationConsole';
 import './styles.css';
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><App /><QualityRecoveryConsole /></React.StrictMode>);
+createRoot(document.getElementById('root')).render(<React.StrictMode><App /><QualityRecoveryConsole /><KeyframeValidationConsole /></React.StrictMode>);
